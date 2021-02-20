1b050cb938c6c3114053e463cffc83bdf3595099
akai <liaokaigogo@foxmail.com>
2021-02-20 11:19:52 +0800

    day day up

--- Array101/04_Search/02_ValidMountainArray.js
+++ Array101/04_Search/02_ValidMountainArray.js
@@ -10,7 +10,33 @@
 const input = [0,3,2,1]
 
 var validMountainArray = function(arr) {
-    
+  if (arr.length < 3){
+    return false
+  }
+  let lM = undefined;
+  for (let i = 0; i < arr.length; i++) { // l -> r
+    console.log(i, arr[i]);
+    let cI = arr[i+1] - arr[i]
+    if (cI < 0) {
+      lM = i
+      break
+    } else if (cI === 0) {
+      return false
+    }
+  }
+  let rM = undefined;
+  for (let j = arr.length-1; j >= 0; j--) { // r -> l
+    let cJ = arr[j] - arr[j+1]
+    if (cJ < 0) {
+      rM = j+1
+      break
+    } else if (cJ === 0) {
+      return false
+    }
+  }
+  // console.log(`leftMax: ${lM}:${arr[lM]},rightMax: ${rM}:${arr[rM]}`);
+
+  return (lM == rM && arr[lM] === arr[rM])
 };
 
 console.log(validMountainArray(input));
